2b8ef89189deb42ea3c597471f797fb40f7b054c
TehNut <tehnut@tehnut.com>
2014-09-10 14:35:20 -0700

    Localize all the things
    -You're welcome translators.
    -I also changed the unlocalized name of the blocks to include the modid
    -Minor edit to main class

--- src/main/java/tombenpotter/sanguimancy/registry/BlocksRegistry.java
+++ src/main/java/tombenpotter/sanguimancy/registry/BlocksRegistry.java
@@ -3,6 +3,7 @@ package tombenpotter.sanguimancy.registry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import tombenpotter.sanguimancy.Sanguimancy;
 import tombenpotter.sanguimancy.blocks.BlockAltarDiviner;
 import tombenpotter.sanguimancy.blocks.BlockAltarEmitter;
 import tombenpotter.sanguimancy.blocks.ItemBlockAltarDiviner;
@@ -15,9 +16,9 @@ public class BlocksRegistry {
 
     public static void registerBlocks() {
         altarDiviner = new BlockAltarDiviner(Material.iron);
-        GameRegistry.registerBlock(altarDiviner, ItemBlockAltarDiviner.class, "BlockAltarDiviner").setBlockName("altarDiviner");
+        GameRegistry.registerBlock(altarDiviner, ItemBlockAltarDiviner.class, "BlockAltarDiviner").setBlockName(Sanguimancy.modid + ".altarDiviner");
 
         altarEmitter = new BlockAltarEmitter(Material.iron);
-        GameRegistry.registerBlock(altarEmitter, ItemBlockAltarEmitter.class, "BlockAltarEmitter").setBlockName("altarEmitter");
+        GameRegistry.registerBlock(altarEmitter, ItemBlockAltarEmitter.class, "BlockAltarEmitter").setBlockName(Sanguimancy.modid + ".altarEmitter");
     }
 }
--- src/main/java/tombenpotter/sanguimancy/registry/RitualRegistry.java
+++ src/main/java/tombenpotter/sanguimancy/registry/RitualRegistry.java
@@ -1,6 +1,7 @@
 package tombenpotter.sanguimancy.registry;
 
 import WayofTime.alchemicalWizardry.api.rituals.Rituals;
+import net.minecraft.util.StatCollector;
 import tombenpotter.sanguimancy.rituals.RitualEffectDrillOfTheDead;
 import tombenpotter.sanguimancy.rituals.RitualEffectLighting;
 import tombenpotter.sanguimancy.rituals.RitualEffectObsidian;
@@ -9,9 +10,9 @@ import tombenpotter.sanguimancy.rituals.RitualEffectTrash;
 public class RitualRegistry {
 
     public static void registerRituals() {
-        Rituals.registerRitual("wellOfTheDeadRitual", 1, 50000, new RitualEffectDrillOfTheDead(), "Drill of the Dead");
-        Rituals.registerRitual("vulcanosFrigiusRitual", 1, 250, new RitualEffectObsidian(), "The Vulcanos Frigius");
-        Rituals.registerRitual("trashRitual", 1, 50, new RitualEffectTrash(), "The Great Deletion");
-        Rituals.registerRitual("illuminationRitual", 1, 300, new RitualEffectLighting(), "The Enlightement");
+        Rituals.registerRitual("wellOfTheDeadRitual", 1, 50000, new RitualEffectDrillOfTheDead(), StatCollector.translateToLocal("ritual.Sanguimancy.drill.dead"));
+        Rituals.registerRitual("vulcanosFrigiusRitual", 1, 250, new RitualEffectObsidian(), StatCollector.translateToLocal("ritual.Sanguimancy.vulcanos.frigius"));
+        Rituals.registerRitual("trashRitual", 1, 50, new RitualEffectTrash(), StatCollector.translateToLocal("ritual.Sanguimancy.great.deletion"));
+        Rituals.registerRitual("illuminationRitual", 1, 300, new RitualEffectLighting(), StatCollector.translateToLocal("ritual.Sanguimancy.enlightenment"));
     }
 }
