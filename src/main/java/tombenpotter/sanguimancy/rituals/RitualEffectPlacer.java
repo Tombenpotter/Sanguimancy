56064d9951c7299a6d4a68e0298428dc8971640f
Tombenpotter <thomas@benguigui.fr>
2014-09-26 21:49:15 +0200

    Some changes to the placing ritual

--- src/main/java/tombenpotter/sanguimancy/rituals/RitualEffectPlacer.java
+++ src/main/java/tombenpotter/sanguimancy/rituals/RitualEffectPlacer.java
@@ -56,9 +56,9 @@ public class RitualEffectPlacer extends RitualEffect {
             for (int i = x - 2; i <= x + 2; i++) {
                 for (int k = z - 2; k <= z + 2; k++) {
                     for (int inv = 0; inv < tileEntity.getSizeInventory(); inv++) {
-                        if (world.getBlock(i, y, k).isReplaceable(world, i, y, k) && tileEntity.getStackInSlot(inv) != null) {
+                        if (world.getBlock(i, y + 1, k).isReplaceable(world, i, y + 1, k) && tileEntity.getStackInSlot(inv) != null) {
                             if (tileEntity.getStackInSlot(inv).getItem() instanceof ItemBlock) {
-                                world.setBlock(i, y, k, Block.getBlockFromItem(tileEntity.getStackInSlot(inv).getItem()), tileEntity.getStackInSlot(inv).getItemDamage(), 3);
+                                world.setBlock(i, y + 1, k, Block.getBlockFromItem(tileEntity.getStackInSlot(inv).getItem()), tileEntity.getStackInSlot(inv).getItemDamage(), 3);
                                 if (tileEntity.getStackInSlot(inv).stackSize > 1) {
                                     tileEntity.getStackInSlot(inv).stackSize = tileEntity.getStackInSlot(inv).stackSize - 1;
                                 } else tileEntity.setInventorySlotContents(inv, null);
