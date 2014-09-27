a9ec5be3e717be72db02a4c7bf0e65112cd6824d
TehNut <tehnut@tehnut.com>
2014-09-27 05:22:29 -0700

    Localize ritual names. Update .gitignore

--- src/main/java/tombenpotter/sanguimancy/registry/RitualRegistry.java
+++ src/main/java/tombenpotter/sanguimancy/registry/RitualRegistry.java
@@ -2,6 +2,7 @@ package tombenpotter.sanguimancy.registry;
 
 import WayofTime.alchemicalWizardry.AlchemicalWizardry;
 import WayofTime.alchemicalWizardry.api.rituals.Rituals;
+import net.minecraft.util.StatCollector;
 import tombenpotter.sanguimancy.entity.EntityChickenMinion;
 import tombenpotter.sanguimancy.entity.EntityPlayerPointer;
 import tombenpotter.sanguimancy.rituals.*;
@@ -9,12 +10,12 @@ import tombenpotter.sanguimancy.rituals.*;
 public class RitualRegistry {
 
     public static void registerRituals() {
-        Rituals.registerRitual("wellOfTheDeadRitual", 1, 50000, new RitualEffectDrillOfTheDead(), "Drill of the Dead");
-        Rituals.registerRitual("vulcanosFrigiusRitual", 1, 250, new RitualEffectObsidian(), "The Vulcanos Frigius");
-        Rituals.registerRitual("trashRitual", 1, 50, new RitualEffectTrash(), "The Great Deletion");
-        Rituals.registerRitual("illuminationRitual", 1, 300, new RitualEffectLighting(), "The Enlightement");
-        Rituals.registerRitual("fellingRitual", 1, 500, new RitualEffectFelling(), "The Timberman");
-        Rituals.registerRitual("placerRitual", 1, 500, new RitualEffectPlacer(), "The Filler");
+        Rituals.registerRitual("wellOfTheDeadRitual", 1, 50000, new RitualEffectDrillOfTheDead(), StatCollector.translateToLocal("ritual.Sanguimancy.drill.dead"));
+        Rituals.registerRitual("vulcanosFrigiusRitual", 1, 250, new RitualEffectObsidian(), StatCollector.translateToLocal("ritual.Sanguimancy.vulcanos.frigius"));
+        Rituals.registerRitual("trashRitual", 1, 50, new RitualEffectTrash(), StatCollector.translateToLocal("ritual.Sanguimancy.trash"));
+        Rituals.registerRitual("illuminationRitual", 1, 300, new RitualEffectLighting(), StatCollector.translateToLocal("ritual.Sanguimancy.illumination"));
+        Rituals.registerRitual("fellingRitual", 1, 500, new RitualEffectFelling(), StatCollector.translateToLocal("ritual.Sanguimancy.feller"));
+        Rituals.registerRitual("placerRitual", 1, 500, new RitualEffectPlacer(), StatCollector.translateToLocal("ritual.Sanguimancy.placer"));
     }
 
     public static void addToWoSBlacklist() {
