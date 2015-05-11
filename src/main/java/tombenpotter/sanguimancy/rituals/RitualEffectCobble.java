bf30db9cdd1d009504d63982b599e665e15d388b
TehNut <nick_ignoffo@yahoo.com>
2015-05-10 22:31:51 -0700

    Swap FIRE and WATER stone placements for Vulcanos Frigius Ritual
    Fixes an issue where the Ritual would activate as Serenade of the Nether.

--- src/main/java/tombenpotter/sanguimancy/rituals/RitualEffectCobble.java
+++ src/main/java/tombenpotter/sanguimancy/rituals/RitualEffectCobble.java
@@ -87,14 +87,14 @@ public class RitualEffectCobble extends RitualEffect {
     @Override
     public List<RitualComponent> getRitualComponentList() {
         ArrayList<RitualComponent> obsidianRitual = new ArrayList();
-        obsidianRitual.add(new RitualComponent(1, 0, 0, RitualComponent.FIRE));
-        obsidianRitual.add(new RitualComponent(-1, 0, 0, RitualComponent.FIRE));
-        obsidianRitual.add(new RitualComponent(-1, 1, -1, RitualComponent.WATER));
-        obsidianRitual.add(new RitualComponent(1, 1, -1, RitualComponent.WATER));
-        obsidianRitual.add(new RitualComponent(0, 0, 1, RitualComponent.FIRE));
-        obsidianRitual.add(new RitualComponent(0, 0, -1, RitualComponent.FIRE));
-        obsidianRitual.add(new RitualComponent(1, 1, 1, RitualComponent.WATER));
-        obsidianRitual.add(new RitualComponent(-1, 1, 1, RitualComponent.WATER));
+        obsidianRitual.add(new RitualComponent(1, 0, 0, RitualComponent.WATER));
+        obsidianRitual.add(new RitualComponent(-1, 0, 0, RitualComponent.WATER));
+        obsidianRitual.add(new RitualComponent(0, 0, 1, RitualComponent.WATER));
+        obsidianRitual.add(new RitualComponent(0, 0, -1, RitualComponent.WATER));
+        obsidianRitual.add(new RitualComponent(-1, 1, -1, RitualComponent.FIRE));
+        obsidianRitual.add(new RitualComponent(1, 1, -1, RitualComponent.FIRE));
+        obsidianRitual.add(new RitualComponent(1, 1, 1, RitualComponent.FIRE));
+        obsidianRitual.add(new RitualComponent(-1, 1, 1, RitualComponent.FIRE));
         return obsidianRitual;
     }
 }
